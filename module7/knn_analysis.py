4b832e75f06cb8a725a8a459222431ea1c6148e4
Brandon Gaither <brandongaither@mail.weber.edu>
2024-11-06 09:39:47 -0700

    Add  Levenshtein documentation

--- module7/knn_analysis.py
+++ module7/knn_analysis.py
@@ -7,7 +7,7 @@ import get_data as gt  # your package
 # Constants
 K = 10  # number of closest matches
 BASE_CASE_ID = 88763  # IMDB_id for 'Back to the Future'
-SECOND_CASE_ID = 89530  # IMDB_id for 'Mad Max Beyond Thunderdome'
+SECOND_CASE_ID = 89530  # IMDB id for 'Mad Max Beyond Thunderdome'
 BASE_YEAR = 1985
 
 
@@ -19,9 +19,7 @@ def print_top_k(df, sorted_value, comparison_type):
     print(f'Top {K} closest matches by {comparison_type}')
     counter = 1
     for idx, row in df.head(K).iterrows():
-        print(
-            f"Top {counter} match: [{idx}]:{row['year']} {row['title']}, {row['genres']}, {row[sorted_value]}]"
-        )
+        print(f"Top {counter} match: [{idx}]: {row['year']} {row['title']}, {row['genres']}, [{row[sorted_value]}]")
         counter += 1
 
 
@@ -37,11 +35,11 @@ def jaccard_similarity_normal(base_case_genres: str, comparator_genres: str):
     return float(numerator)/float(denominator)
 
 
-def get_weighted_jaccard_similarity_dict(df):
-    # Get our selections of our BASE_CASE and SECOND_CASE_ID
+def _get_weighted_jaccard_similarity_dict(df):
+    # Get our selections of our BASE_CASE_ID and SECOND_CASE_ID
     selections_df = [df.loc[BASE_CASE_ID], df.loc[SECOND_CASE_ID]]
     # Add weights for the similarity index
-    genres_weighted_dictionary = {'total':0}
+    genres_weighted_dictionary = {'total': 0}
     for movie in selections_df:
         for genre in movie['genres'].split(';'):
             if genre in genres_weighted_dictionary:
@@ -54,7 +52,7 @@ def get_weighted_jaccard_similarity_dict(df):
 
 
 def jaccard_similarity_weighted(df: pd.DataFrame, comparator_genre: str):
-    weighted_dictionary = get_weighted_jaccard_similarity_dict(df)
+    weighted_dictionary = _get_weighted_jaccard_similarity_dict(df)
     numerator = 0
     denominator = weighted_dictionary['total']
     for genre in comparator_genre.split(';'):
@@ -62,7 +60,6 @@ def jaccard_similarity_weighted(df: pd.DataFrame, comparator_genre: str):
             numerator += weighted_dictionary[genre]
 
     return float(numerator)/float(denominator)
-
 
 
 def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
@@ -74,19 +71,19 @@ def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted
     else:
         df[sorted_value] = df[comparison_type].map(
             lambda x: metric_func(base_case[comparison_type], x))
-    
+
     # Sort return values from function stub
-    # Jaccard needs to be sorted in descending order
+    # Jaccard needs to sorted in descending order
     if 'jaccard' in metric_func.__name__:
         sorted_df = df.sort_values(by=sorted_value, ascending=False)
     else:
         # default is ascending = True
         sorted_df = df.sort_values(by=sorted_value)
-    
-    sorted_df = df.sort_values(by=sorted_value)
+
     sorted_df.drop(BASE_CASE_ID, inplace=True)  # drop base case
 
-    print_top_k(sorted_df, sorted_value, comparison_type) # print return values 
+    # print return values
+    print_top_k(sorted_df, sorted_value, comparison_type)
 
 
 def main():
@@ -115,24 +112,22 @@ def main():
                         sorted_value='euclidean_distance')
     # Task 5: Jaccard Similarity
     print(f'\nTask 5:KNN Analysis with Jaccard Similarity Normal')
-    data = data[data['year'] >= BASE_YEAR] # Add filter
+    data = data[data['year'] >= BASE_YEAR]  # Add filter
     knn_analysis_driver(data_df=data, base_case=base_case,
                         comparison_type='genres', metric_func=jaccard_similarity_normal,
                         sorted_value='jaccard_similarity')
-    # Task 6: Jaccard Similarity Weighted
+    # Task 6: Jaccard Weighted Similarity
     print(f'\nTask 6:KNN Analysis with Jaccard Similarity Weighted')
     base_case = data.loc[BASE_CASE_ID]
     second_case = data.loc[SECOND_CASE_ID]
-    print(f"Comparing all movies to our base case:[{base_case['title']}] and [{second_case['title']}]")
-    # Add a second filter: rating ['G', 'PG', 'PG-13']
-    # Add a third filter: stars >= 5
-    data = data[data['year'] >= BASE_YEAR] # Add filter
-    data = data[(data['stars'] >= 5) & (data['rating'].isin(['G', 'PG', 'PG-13']))] # Stars and Rating filter
-
+    print(f"Comparing all movies to our base case: [{base_case['title']}] and [{second_case['title']}]")
+    # Add a second filter: rating ['G', 'PG', 'PG-13'],  Add a third filter: stars >= 5
+    data = data[data['year'] >= BASE_YEAR]  # Year filter
+    data = data[(data['stars'] >= 5) & (data['rating'].isin(['G', 'PG', 'PG-13']))]  # Stars and Rating filter
     knn_analysis_driver(data_df=data, base_case=base_case,
                         comparison_type='genres', metric_func=jaccard_similarity_weighted,
                         sorted_value='jaccard_similarity_weighted')
-    
+
 
 if __name__ == '__main__':
     main()
